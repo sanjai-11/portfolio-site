7b3b3eab14410527f6e3e5a66291265b20436c77
Sanjai Bala <115383867+sanjai-11@users.noreply.github.com>
2025-07-21 12:51:17 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // API Configuration for GitHub Pages
 const API_CONFIG = {
-    GEMINI_API_KEY: 'AIzaSyClLs1UPwwpusFerNd71s04Ez3CK5HS6mA'
+    GEMINI_API_KEY: 'AIzaSyD3S6L8Q06zIGkDUuDXKaMDQQirVLIBP1E'
 // 'AIzaSyBFw3iLeokV_PO82IoMIO7-9MpU2-ulH74'
 }; 
